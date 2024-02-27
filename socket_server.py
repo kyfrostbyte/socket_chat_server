96a8d429a15ee5c885982b05b69ed7b1636b56a3
kyfrostbyte <94023660+kyfrostbyte@users.noreply.github.com>
2024-02-26 18:20:12 -0700

    Finished formatting

--- socket_server.py
+++ socket_server.py
@@ -9,15 +9,13 @@ HEADER_LENGTH = 10
 # Create an instance of the socket and set correct attributes
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Defines relationship between socket and address
 server_socket.bind((IP_address, port))
 
 # Instruct the server to be prepared to accept client connection requests
 server_socket.listen()
 print(f"Now listening on: {IP_address}")
 
-# Creates a list to manage the list of clients (Each client is its own socket).
+# Creates a list to manage the all sockets (server + each client)
 sockets_list = [server_socket]
 
 # Create a dictionary to contain client information. Key = client socket, value = userdata
@@ -38,6 +36,7 @@ def receive_message(client_socket):
 
     # Handle unexpected errors
     except:
+        print("Bad message")
         return False
 
 
@@ -46,10 +45,9 @@ while True:
     # Parameters are sockets that need to read from, written to, and potential errors
     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
-
-    for notified_socket in read_sockets:
+    for socket in read_sockets:
         # Handle new connections
-        if notified_socket == server_socket:
+        if socket == server_socket:
             client_socket, client_address = server_socket.accept()
 
             user = receive_message(client_socket)
@@ -61,29 +59,29 @@ while True:
             # Add new connections to socket list and client dictionary
             sockets_list.append(client_socket)
             clients[client_socket] = user
-            print(f"Accepted new connection from: {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}")
+            print(
+                f"Accepted new connection from: {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}")
 
         # Handle data from existing connections
         else:
-            message = receive_message(notified_socket)
+            message = receive_message(socket)
 
             # If user disconnects, remove user from socket_list
             if message is False:
-                print(f"Connection closed from {clients[notified_socket]['data'].decode('utf-8')}")
-                sockets_list.remove(notified_socket)
-                del clients[notified_socket]
+                print(f"Connection closed from {clients[socket]['data'].decode('utf-8')}")
+                sockets_list.remove(socket)
+                del clients[socket]
                 continue
 
-            user = clients[notified_socket]
+            user = clients[socket]
             print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
 
             for client_socket in clients:
                 # For all users that are not the user that sent the message, send them the message
-                if client_socket != notified_socket:
+                if client_socket != socket:
                     client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
 
     # Handle sockets that throw an error
-    for notified_socket in exception_sockets:
-        sockets_list.remove(notified_socket)
-        del clients[notified_socket]
-
+    for socket in exception_sockets:
+        sockets_list.remove(socket)
+        del clients[socket]
